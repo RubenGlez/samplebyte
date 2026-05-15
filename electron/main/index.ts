f4f5c6b51ee67430ab935c36019bb3f4a110711a
RubenGlez <ruben.daren@gmail.com>
2026-05-15 16:31:27 +0200

    fix: allow audio fetches in production CSP

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -109,7 +109,7 @@ app.whenReady().then(() => {
       callback({
         responseHeaders: {
           ...details.responseHeaders,
-          'Content-Security-Policy': ["default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; media-src 'self' file: local-file: https://cdn.freesound.org; img-src 'self' data:"],
+          'Content-Security-Policy': ["default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' local-file: blob: https://freesound.org https://cdn.freesound.org; media-src 'self' file: local-file: blob: https://cdn.freesound.org; img-src 'self' data:"],
         },
       })
     })
